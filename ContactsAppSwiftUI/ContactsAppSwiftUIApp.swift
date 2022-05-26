7f9b9385526f781e83866e2d04f0f1b43fc6a458
back2 <back2@local.ru>
2022-05-26 11:18:59 +0300

    Initial Commit

---
+++ ContactsAppSwiftUI/ContactsAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppSwiftUIApp.swift
+//  ContactsAppSwiftUI
+//
+//  Created by Ivan on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
